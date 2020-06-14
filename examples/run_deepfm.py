e9fc765796e54a4b75670a6d9a7d54495054c600
imsheridan <xiaoyudong0512@gmail.com>
2020-06-14 21:18:06 +0800

    fix run_deepfm example

--- examples/run_deepfm.py
+++ examples/run_deepfm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python 
 #coding=utf-8
 """
 Author:deepdeliver
@@ -13,67 +13,66 @@ sys.path.append("../")
 
 from xdeeprank.models import DeepFM
 from xdeeprank.inputs import input_fn
-
-FLAGS = tf.app.flags.FLAGS
+from sys import argv
 
 def main(_):
     # init environments
-    utils.init_cmd_args()
+    args_parser = utils.init_cmd_args(argv)
 
-    tr_files = glob.glob("%s/tr*libsvm" % FLAGS.data_dir)
+    tr_files = glob.glob("%s/tr*libsvm" % args_parser.data_dir)
     random.shuffle(tr_files)
     print("tr_files:", tr_files)
-    va_files = glob.glob("%s/va*libsvm" % FLAGS.data_dir)
+    va_files = glob.glob("%s/va*libsvm" % args_parser.data_dir)
     print("va_files:", va_files)
-    te_files = glob.glob("%s/te*libsvm" % FLAGS.data_dir)
+    te_files = glob.glob("%s/te*libsvm" % args_parser.data_dir)
     print("te_files:", te_files)
 
-    if FLAGS.clear_existing_model:
+    if args_parser.clear_existing_model:
         try:
-            shutil.rmtree(FLAGS.model_dir)
+            shutil.rmtree(args_parser.model_dir)
         except Exception as e:
             print(e, "at clear_existing_model")
         else:
-            print("existing model cleaned at %s" % FLAGS.model_dir)
+            print("existing model cleaned at %s" % args_parser.model_dir)
 
-    utils.set_dist_env()
+    utils.set_dist_env(args_parser)
 
     # build model params
     model_params = {
-        "field_size": FLAGS.field_size,
-        "feature_size": FLAGS.feature_size,
-        "embedding_size": FLAGS.embedding_size,
-        "learning_rate": FLAGS.learning_rate,
-        "batch_norm": FLAGS.batch_norm,
-        "batch_norm_decay": FLAGS.batch_norm_decay,
-        "l2_reg": FLAGS.l2_reg,
-        "deep_layers": FLAGS.deep_layers,
-        "dropout": FLAGS.dropout,
-        "optimizer": FLAGS.optimizer
+        "field_size": args_parser.field_size,
+        "feature_size": args_parser.feature_size,
+        "embedding_size": args_parser.embedding_size,
+        "learning_rate": args_parser.learning_rate,
+        "batch_norm": args_parser.batch_norm,
+        "batch_norm_decay": args_parser.batch_norm_decay,
+        "l2_reg": args_parser.l2_reg,
+        "deep_layers": args_parser.deep_layers,
+        "dropout": args_parser.dropout,
+        "optimizer": args_parser.optimizer
     }
 
-    config = tf.estimator.RunConfig().replace(session_config = tf.ConfigProto(device_count={'GPU':0, 'CPU':FLAGS.num_threads}),
-            log_step_count_steps=FLAGS.log_steps, save_summary_steps=FLAGS.log_steps)
-    model = tf.estimator.Estimator(model_fn=DeepFM, model_dir=FLAGS.model_dir, params=model_params, config=config)
+    config = tf.estimator.RunConfig().replace(session_config = tf.ConfigProto(device_count={'GPU':0, 'CPU':args_parser.num_threads}),
+            log_step_count_steps=args_parser.log_steps, save_summary_steps=args_parser.log_steps)
+    model = tf.estimator.Estimator(model_fn=DeepFM, model_dir=args_parser.model_dir, params=model_params, config=config)
 
-    if FLAGS.task_type == 'train':
-        train_spec = tf.estimator.TrainSpec(input_fn=lambda: input_fn(tr_files, num_epochs=FLAGS.num_epochs, batch_size=FLAGS.batch_size))
-        eval_spec = tf.estimator.EvalSpec(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size), steps=None, start_delay_secs=1000, throttle_secs=1200)
+    if args_parser.task_type == 'train':
+        train_spec = tf.estimator.TrainSpec(input_fn=lambda: input_fn(tr_files, num_epochs=args_parser.num_epochs, batch_size=args_parser.batch_size))
+        eval_spec = tf.estimator.EvalSpec(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=args_parser.batch_size), steps=None, start_delay_secs=1000, throttle_secs=1200)
         tf.estimator.train_and_evaluate(model, train_spec, eval_spec)
-    elif FLAGS.task_type == 'eval':
-        model.evaluate(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size))
-    elif FLAGS.task_type == 'infer':
-        preds = model.predict(input_fn=lambda: input_fn(te_files, num_epochs=1, batch_size=FLAGS.batch_size), predict_keys="prob")
-        with open(FLAGS.data_dir+"/pred.txt", "w") as fo:
+    elif args_parser.task_type == 'eval':
+        model.evaluate(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=args_parser.batch_size))
+    elif args_parser.task_type == 'infer':
+        preds = model.predict(input_fn=lambda: input_fn(te_files, num_epochs=1, batch_size=args_parser.batch_size), predict_keys="prob")
+        with open(args_parser.data_dir+"/pred.txt", "w") as fo:
             for prob in preds:
                 fo.write("%f\n" % (prob['prob']))
-    elif FLAGS.task_type == 'export':
+    elif args_parser.task_type == 'export':
         feature_spec = {
-            'feat_ids': tf.placeholder(dtype=tf.int64, shape=[None, FLAGS.field_size], name='feat_ids'),
-            'feat_vals': tf.placeholder(dtype=tf.float32, shape=[None, FLAGS.field_size], name='feat_vals')
+            'feat_ids': tf.placeholder(dtype=tf.int64, shape=[None, args_parser.field_size], name='feat_ids'),
+            'feat_vals': tf.placeholder(dtype=tf.float32, shape=[None, args_parser.field_size], name='feat_vals')
         }
         serving_input_receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feature_spec)
-        model.export_savedmodel(FLAGS.servable_model_dir, serving_input_receiver_fn)
+        model.export_savedmodel(args_parser.servable_model_dir, serving_input_receiver_fn)
 
 if __name__ == "__main__":
     tf.logging.set_verbosity(tf.logging.INFO)
--- examples/utils.py
+++ examples/utils.py
@@ -6,67 +6,71 @@ Author:deepdeliver
 
 import tensorflow as tf
 from datetime import date, timedelta
+from sys import argv
+import argparse
 
-FLAGS = tf.app.flags.FLAGS
+parser = argparse.ArgumentParser()
 
-def init_cmd_args():
+def init_cmd_args(args):
     # Initialize cmd arguments
-    tf.app.flags.DEFINE_integer("dist_mode", 0, "distribuion mode {0-loacal, 1-single_dist, 2-multi_dist}")
-    tf.app.flags.DEFINE_string("ps_hosts", '', "Comma-separated list of hostname:port pairs")
-    tf.app.flags.DEFINE_string("worker_hosts", '', "Comma-separated list of hostname:port pairs")
-    tf.app.flags.DEFINE_string("job_name", '', "One of 'ps', 'worker'")
-    tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
-    tf.app.flags.DEFINE_integer("num_threads", 16, "Number of threads")
-    tf.app.flags.DEFINE_integer("feature_size", 0, "Number of features")
-    tf.app.flags.DEFINE_integer("field_size", 0, "Number of fields")
-    tf.app.flags.DEFINE_integer("embedding_size", 32, "Embedding size")
-    tf.app.flags.DEFINE_integer("num_epochs", 10, "Number of epochs")
-    tf.app.flags.DEFINE_integer("batch_size", 64, "Number of batch size")
-    tf.app.flags.DEFINE_integer("log_steps", 1000, "save summary every steps")
-    tf.app.flags.DEFINE_float("learning_rate", 0.0005, "learning rate")
-    tf.app.flags.DEFINE_float("l2_reg", 0.0001, "L2 regularization")
-    tf.app.flags.DEFINE_string("loss_type", 'log_loss', "loss type {square_loss, log_loss}")
-    tf.app.flags.DEFINE_string("optimizer", 'Adam', "optimizer type {Adam, Adagrad, GD, Momentum}")
-    tf.app.flags.DEFINE_string("deep_layers", '256,128,64', "deep layers")
-    tf.app.flags.DEFINE_string("dropout", '0.5,0.5,0.5', "dropout rate")
-    tf.app.flags.DEFINE_boolean("batch_norm", False, "perform batch normaization (True or False)")
-    tf.app.flags.DEFINE_float("batch_norm_decay", 0.9, "decay for the moving average(recommend trying decay=0.9)")
-    tf.app.flags.DEFINE_string("data_dir", '', "data dir")
-    tf.app.flags.DEFINE_string("dt_dir", '', "data dt partition")
-    tf.app.flags.DEFINE_string("model_dir", '', "model check point dir")
-    tf.app.flags.DEFINE_string("servable_model_dir", '', "export servable model for TensorFlow Serving")
-    tf.app.flags.DEFINE_string("task_type", 'train', "task type {train, infer, eval, export}")
-    tf.app.flags.DEFINE_boolean("clear_existing_model", False, "clear existing model or not")
+    parser.add_argument("--dist_mode", type=int, default=0, help="distribuion mode {0-loacal, 1-single_dist, 2-multi_dist}")
+    parser.add_argument("--ps_hosts", type=str, default='', help="Comma-separated list of hostname:port pairs")
+    parser.add_argument("--worker_hosts", type=str, default='', help="Comma-separated list of hostname:port pairs")
+    parser.add_argument("--job_name", type=str, default='', help="One of 'ps', 'worker'")
+    parser.add_argument("--task_index", type=int, default=0, help="Index of task within the job")
+    parser.add_argument("--num_threads", type=int, default=16, help="Number of threads")
+    parser.add_argument("--feature_size", type=int, default=0, help="Number of features")
+    parser.add_argument("--field_size", type=int, default=0, help="Number of fields")
+    parser.add_argument("--embedding_size", type=int, default=32, help="Embedding size")
+    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs")
+    parser.add_argument("--batch_size", type=int, default=64, help="Number of batch size")
+    parser.add_argument("--log_steps", type=int, default=1000, help="save summary every steps")
+    parser.add_argument("--learning_rate", type=float, default=0.0005, help="learning rate")
+    parser.add_argument("--l2_reg", type=float, default=0.0001, help="L2 regularization")
+    parser.add_argument("--loss_type", type=str, default='log_loss', help="loss type {square_loss, log_loss}")
+    parser.add_argument("--optimizer", type=str, default='Adam', help="optimizer type {Adam, Adagrad, GD, Momentum}")
+    parser.add_argument("--deep_layers", type=str, default='256,128,64', help="deep layers")
+    parser.add_argument("--dropout", type=str, default='0.5,0.5,0.5', help="dropout rate")
+    parser.add_argument("--batch_norm", type=bool, default=False, help="perform batch normaization (True or False)")
+    parser.add_argument("--batch_norm_decay", type=float, default=0.9, help="decay for the moving average(recommend trying decay=0.9)")
+    parser.add_argument("--data_dir", type=str, default='', help="data dir")
+    parser.add_argument("--dt_dir", type=str, default='', help="data dt partition")
+    parser.add_argument("--model_dir", type=str, default='', help="model check point dir")
+    parser.add_argument("--servable_model_dir", type=str, default='', help="export servable model for TensorFlow Serving")
+    parser.add_argument("--task_type", type=str, default='train', help="task type {train, infer, eval, export}")
+    parser.add_argument("--clear_existing_model", type=bool, default=False, help="clear existing model or not")
+    args_parser = parser.parse_args(args[1:])
 
     # check arguments
-    if FLAGS.dt_dir == "":
-        FLAGS.dt_dir = (date.today() + timedelta(-1)).strftime('%Y%m%d')
-    FLAGS.model_dir = FLAGS.model_dir + FLAGS.dt_dir
+    if args_parser.dt_dir == "":
+        args_parser.dt_dir = (date.today() + timedelta(-1)).strftime('%Y%m%d')
+    args_parser.model_dir = args_parser.model_dir + args_parser.dt_dir
 
-    print('task_type ', FLAGS.task_type)
-    print('model_dir ', FLAGS.model_dir)
-    print('data_dir ', FLAGS.data_dir)
-    print('dt_dir ', FLAGS.dt_dir)
-    print('num_epochs ', FLAGS.num_epochs)
-    print('feature_size ', FLAGS.feature_size)
-    print('field_size ', FLAGS.field_size)
-    print('embedding_size ', FLAGS.embedding_size)
-    print('batch_size ', FLAGS.batch_size)
-    print('deep_layers ', FLAGS.deep_layers)
-    print('dropout ', FLAGS.dropout)
-    print('loss_type ', FLAGS.loss_type)
-    print('optimizer ', FLAGS.optimizer)
-    print('learning_rate ', FLAGS.learning_rate)
-    print('batch_norm_decay ', FLAGS.batch_norm_decay)
-    print('batch_norm ', FLAGS.batch_norm)
-    print('l2_reg ', FLAGS.l2_reg)
+    print('task_type ', args_parser.task_type)
+    print('model_dir ', args_parser.model_dir)
+    print('data_dir ', args_parser.data_dir)
+    print('dt_dir ', args_parser.dt_dir)
+    print('num_epochs ', args_parser.num_epochs)
+    print('feature_size ', args_parser.feature_size)
+    print('field_size ', args_parser.field_size)
+    print('embedding_size ', args_parser.embedding_size)
+    print('batch_size ', args_parser.batch_size)
+    print('deep_layers ', args_parser.deep_layers)
+    print('dropout ', args_parser.dropout)
+    print('loss_type ', args_parser.loss_type)
+    print('optimizer ', args_parser.optimizer)
+    print('learning_rate ', args_parser.learning_rate)
+    print('batch_norm_decay ', args_parser.batch_norm_decay)
+    print('batch_norm ', args_parser.batch_norm)
+    print('l2_reg ', args_parser.l2_reg)
+    return args_parser
 
-def set_dist_env():
-    if FLAGS.dist_mode == 1:        # 本地分布式测试模式1 chief, 1 ps, 1 evaluator
-        ps_hosts = FLAGS.ps_hosts.split(',')
-        chief_hosts = FLAGS.chief_hosts.split(',')
-        task_index = FLAGS.task_index
-        job_name = FLAGS.job_name
+def set_dist_env(args_parser):
+    if args_parser.dist_mode == 1:        # 本地分布式测试模式1 chief, 1 ps, 1 evaluator
+        ps_hosts = args_parser.ps_hosts.split(',')
+        chief_hosts = args_parser.chief_hosts.split(',')
+        task_index = args_parser.task_index
+        job_name = args_parser.job_name
         print('ps_host', ps_hosts)
         print('chief_hosts', chief_hosts)
         print('job_name', job_name)
@@ -78,13 +82,13 @@ def set_dist_env():
         }
         print(json.dumps(tf_config))
         os.environ['TF_CONFIG'] = json.dumps(tf_config)
-    elif FLAGS.dist_mode == 2:      # 集群分布式模式
-        ps_hosts = FLAGS.ps_hosts.split(',')
-        worker_hosts = FLAGS.worker_hosts.split(',')
+    elif args_parser.dist_mode == 2:      # 集群分布式模式
+        ps_hosts = args_parser.ps_hosts.split(',')
+        worker_hosts = args_parser.worker_hosts.split(',')
         chief_hosts = worker_hosts[0:1] # get first worker as chief
         worker_hosts = worker_hosts[2:] # the rest as worker
-        task_index = FLAGS.task_index
-        job_name = FLAGS.job_name
+        task_index = args_parser.task_index
+        job_name = args_parser.job_name
         print('ps_host', ps_hosts)
         print('worker_host', worker_hosts)
         print('chief_hosts', chief_hosts)
